58d910023f3c45990980f6d4d0326c2e5ae70a0f
Jacob Roberts <jacoberts@gmail.com>
2023-02-01 19:14:19 -0800

    Initial commit of a semi working streamlit app.

---
+++ streamlit.py
@@ -0,0 +1,195 @@
+from typing import Dict, List
+import streamlit as st
+import pandas as pd
+from io import StringIO
+import re
+
+import numpy
+numpy.random.seed(123) #This will ensure that the result of the optimization is always the same
+
+from dnachisel import *
+from Bio import Seq, SeqIO
+
+
+COCOPUTS_DB_FNAME = '220916_codon_analysis/o537-Refseq_species.tsv'
+
+@st.experimental_memo 
+def get_cocoput_organism_list():
+  # df = pd.read_csv('220916_codon_analysis/220926_genome_codons.tsv', sep='\t', index_col=False) 
+  df = pd.read_csv(COCOPUTS_DB_FNAME, sep='\t', index_col=False) 
+  # df = pd.read_csv('220916_codon_analysis/o537-genbank_species.tsv', sep='\t', index_col=False)
+  return df.apply(
+    lambda r: f"{r['Species']} (TaxID: {r['Taxid']})", axis=1
+  ).unique().tolist()
+
+
+def get_taxid_from_cocoput_name(cocoput_name):
+  rematch = re.match(r'.*\(TaxID: (\d+)\)', cocoput_name)
+  assert rematch, f'Somehow the cocoput name was poorly formatted, {cocoput_name}'
+  return int(rematch.groups()[0])
+
+
+AA_TO_CODON: Dict[str, List[str]] = {
+    "*": ["TAA", "TAG", "TGA"],
+    "A": ["GCA", "GCC", "GCG", "GCT"],
+    "C": ["TGC", "TGT"],
+    "D": ["GAC", "GAT"],
+    "E": ["GAA", "GAG"],
+    "F": ["TTC", "TTT"],
+    "G": ["GGA", "GGC", "GGG", "GGT"],
+    "H": ["CAC", "CAT"],
+    "I": ["ATA", "ATC", "ATT"],
+    "K": ["AAA", "AAG"],
+    "L": ["CTA", "CTC", "CTG", "CTT", "TTA", "TTG"],
+    "M": ["ATG"],
+    "N": ["AAC", "AAT"],
+    "P": ["CCA", "CCC", "CCG", "CCT"],
+    "Q": ["CAA", "CAG"],
+    "R": ["AGA", "AGG", "CGA", "CGC", "CGG", "CGT"],
+    "S": ["AGC", "AGT", "TCA", "TCC", "TCG", "TCT"],
+    "T": ["ACA", "ACC", "ACG", "ACT"],
+    "V": ["GTA", "GTC", "GTG", "GTT"],
+    "W": ["TGG"],
+    "Y": ["TAC", "TAT"],
+}
+
+
+def convert_cocoputs_table_to_dnachisel(
+    codon_table_counts: dict,
+) -> Dict[str, Dict[str, float]]:
+    new_codon_table: Dict[str, Dict[str, float]] = {}
+    for aa in AA_TO_CODON:
+        new_codon_table[aa] = {}
+        codon_sum: int = sum(
+            [codon_table_counts[codon] for codon in AA_TO_CODON[aa]]
+        )
+        for codon in AA_TO_CODON[aa]:
+            new_codon_table[aa][codon] = round(
+                codon_table_counts[codon] / codon_sum, 3
+            )
+    return new_codon_table
+
+
+@st.experimental_memo
+def get_codon_table_for_taxid(taxid):
+  df = pd.read_csv(COCOPUTS_DB_FNAME, sep='\t', index_col=False)
+  subset = df[df.Taxid == taxid]
+  row = subset[subset['# CDS'] == subset['# CDS'].max()].iloc[0]
+  codons = [a + b + c for a in 'ATCG' for b in 'ATCG' for c in 'ATCG']
+  codon_table = {k: v for k, v in row.to_dict().items() if k in codons}
+  return convert_cocoputs_table_to_dnachisel(codon_table)
+    
+
+st.set_page_config(page_title="Recoder")
+st.header('''Recoder''')
+st.write('''
+Recode genes for your target organism.
+
+Enter your native coding sequence below.
+''')
+
+col1, col2 = st.columns(2)
+
+with col1:
+  optimization_method = st.radio(
+    "Optimization Method",
+    ["use_best_codon", "match_codon_usage", "harmonize_rca"],
+    key="visibility",
+    # help='Choose the optimization method, harmonize_rca is recommended.'
+    # label_visibility=st.session_state.visibility,
+    # disabled=st.session_state.disabled,
+    # horizontal=st.session_state.horizontal,
+  )
+
+with col2:
+  target_organism = st.selectbox(
+    'target organism',
+    get_cocoput_organism_list()
+  )
+  target_taxid = get_taxid_from_cocoput_name(target_organism)
+  target_coding_table = get_codon_table_for_taxid(target_taxid)
+
+  if optimization_method == 'harmonize_rca':
+    source_organism = st.selectbox(
+      'source organism',
+      get_cocoput_organism_list()
+    )
+    source_taxid = get_taxid_from_cocoput_name(source_organism)
+    source_coding_table = get_codon_table_for_taxid(source_taxid)
+  else:
+    source_coding_table = None
+
+
+original_fasta_str = st.text_area(
+  'native coding sequence',
+  '>gene\nATGAGTAGT',
+  help=f'Input the native coding sequence.'
+)
+with StringIO(original_fasta_str) as fio:
+  records = list(SeqIO.parse(fio, 'fasta'))
+if len(records) == 0:
+  st.warning(f'Found zero valid records in text box, should have one.')
+  st.stop()
+
+with st.expander('Advanced Settings'):
+  st.write('Advanced settings here')
+  
+# Do some input validation.
+if not target_coding_table:
+  st.warning('Must specify a target organism.')
+  st.stop()
+if optimization_method == 'harmonize_rca':
+  if not source_coding_table:
+    st.warning('Must specify a source organism if using harmonize_rca method.')
+    st.stop()
+  codon_optimize_kwargs = {
+    'original_codon_usage_table': source_coding_table
+  }
+else:
+  codon_optimize_kwargs = {}
+
+constraints_logs = []
+objectives_logs = []
+recodings = []
+try:
+  for record in records:
+    problem = DnaOptimizationProblem(
+        sequence=record,
+        constraints=[
+            UniquifyAllKmers(10, include_reverse_complement=True),
+            AvoidHairpins(stem_size=10,hairpin_window=100),
+            AvoidPattern("9xA"),
+            AvoidPattern("9xT"),
+            AvoidPattern("6xC"),
+            AvoidPattern("6xG"),
+            AvoidPattern("NdeI_site"),
+            AvoidPattern("XhoI_site"),
+            AvoidPattern("SpeI_site"),
+            AvoidPattern("BamHI_site"),
+            AvoidPattern("BsaI_site"),
+            EnforceGCContent(mini=0.3, maxi=0.75, window=50),
+            EnforceTranslation()
+        ],
+        objectives=[CodonOptimize(method=optimization_method, codon_usage_table=target_coding_table, **codon_optimize_kwargs)]
+    )
+
+    problem.max_random_iters = 10000
+    problem.resolve_constraints()
+    problem.optimize()
+    
+    constraints_logs.append(problem.constraints_text_summary())
+    objectives_logs.append(problem.objectives_text_summary())
+    recodings.append(problem.sequence)
+
+except Exception as e:
+  st.warning(e)
+  st.stop()
+  
+
+with st.expander('DNA Chisel Logs'):
+  for log in constraints_logs + objectives_logs:
+    st.text(log)
+
+for record, recoding in zip(records, recodings):
+  st.write(record.id)
+  st.write(recoding)
